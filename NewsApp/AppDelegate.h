9980bb3c07b52eb635fa7446e0c5ef6d77dd0f53
shiguangyin <shiguangyin928@gmail.com>
2019-04-20 20:57:44 +0800

    Initial Commit

---
+++ NewsApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewsApp
+//
+//  Created by Masker on 2019/4/20.
+//  Copyright © 2019 Masker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
