fba63cea44f3ad60f0ca982344d5a76d03d8f0bd
shiguangyin <shiguangyin928@gmail.com>
2019-05-18 17:18:01 +0800

    Add UIImageView

--- NewsApp/Home/NewsTableViewCell.m
+++ NewsApp/Home/NewsTableViewCell.m
@@ -16,6 +16,7 @@
 
 @property (strong, nonatomic) UILabel *timeLabel;
 
+@property (strong, nonatomic) UIImageView *contentImage;
 
 @end
 
@@ -25,6 +26,7 @@
 - (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(nullable NSString *)reuseIdentifier {
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
+        self.contentView.backgroundColor = [UIColor lightGrayColor];
         [self.contentView addSubview:({
             self.titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 20, 0, 0)];
             self.titleLabel.font = [UIFont systemFontOfSize:16];
@@ -34,24 +36,42 @@
         [self.contentView addSubview:({
             self.sourceLabel = [[UILabel alloc] init];
             self.sourceLabel.textColor = [UIColor grayColor];
+            self.sourceLabel.font = [UIFont systemFontOfSize:14];
             self.sourceLabel;
         })];
 
         [self.contentView addSubview:({
             self.commentLabel = [[UILabel alloc] init];
             self.commentLabel.textColor = [UIColor grayColor];
+            self.commentLabel.font = [UIFont systemFontOfSize:14];
             self.commentLabel;
         })];
 
         [self.contentView addSubview:({
             self.timeLabel = [[UILabel alloc] init];
             self.timeLabel.textColor = [UIColor grayColor];
+            self.timeLabel.font = [UIFont systemFontOfSize:14];
             self.timeLabel;
+        })];
+
+        [self.contentView addSubview:({
+            self.contentImage = [[UIImageView alloc] init];
+            self.contentImage.backgroundColor = [UIColor redColor];
+            self.contentImage;
         })];
     }
 
     return self;
 }
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    CGFloat width = 100;
+    CGFloat height = 60;
+    CGFloat x = self.contentView.width - width - 20;
+    self.contentImage.frame = CGRectMake(x, 20, width, height);;
+}
+
 
 - (void)bindData {
     self.titleLabel.text = @"News title";
@@ -74,12 +94,12 @@
 
 
     [self.commentLabel sizeToFit];
-    self.commentLabel.frame = CGRectMake(self.sourceLabel.x + self.sourceLabel.width + 20, titleBottom + 20,
+    self.commentLabel.frame = CGRectMake(self.sourceLabel.x + self.sourceLabel.width + 10, titleBottom + 20,
             self.commentLabel.width, self.commentLabel.height);
 
     self.timeLabel.text = @"Time";
     [self.timeLabel sizeToFit];
-    self.timeLabel.frame = CGRectMake(self.commentLabel.x + self.commentLabel.width + 20, titleBottom + 20,
+    self.timeLabel.frame = CGRectMake(self.commentLabel.x + self.commentLabel.width + 10, titleBottom + 20,
             self.titleLabel.width, self.titleLabel.height);
 
 
